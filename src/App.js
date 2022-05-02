b10692abc413af7ae1fff25d3758d07d699cc5a6
berkagmpp <berkagmpp@gmail.com>
2022-05-02 18:27:50 +1200

    add allowToggle Button and put dependancy to useCallback() of toggle fn in App.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,27 @@ import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
     const [showParagraph, setShowParagraph] = useState(false);
+    const [allowToggle, setAllowToggle] = useState(false);
 
     console.log('App running: every state change entire re-executed, re-evaluated.')
     // this message always appeared in console because DemeOutput state is in App.js -> re-eval, re-exc
 
     const toggleParagraphHandler = useCallback(() => {
-        setShowParagraph((prevShowParagraph) => !prevShowParagraph) // clean way to set oposit state
-    }, []); 
+        if (allowToggle) {
+            setShowParagraph(prevShowParagraph => !prevShowParagraph); // clean way to set oposit state
+        };
+    }, [allowToggle]); 
+
+    const allowToggleHandler = () => {
+        setAllowToggle(true);
+    }; 
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={false} /> 
-            <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
+            <DemoOutput show={showParagraph} /> 
+            <Button onClick={toggleParagraphHandler}>Allow Toggling!</Button>
+            <Button onClick={allowToggleHandler}>Toggle Paragraph!</Button>
         </div>
     );
 }
