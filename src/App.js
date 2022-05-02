d76ce6a18c4e00727249439ad41a111d1b69d50b
berkagmpp <berkagmpp@gmail.com>
2022-05-02 15:00:09 +1200

    export default React.memo(child-component): optimise (avoid unnecessary rendering)

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import './App.css';
 import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
-    const [showParagraph, setShoeParagraph] = useState(false);
+    const [showParagraph, setShowParagraph] = useState(false);
 
-    console.log('App running: every state change entire re-excuted, re-evaluated.')
-    // this message always appeared in cosole because DemeOutput state is in App.js -> re-eval, re-exc
+    console.log('App running: every state change entire re-executed, re-evaluated.')
+    // this message always appeared in console because DemeOutput state is in App.js -> re-eval, re-exc
 
     const toggleParagraphHandler = event => {
-        setShoeParagraph(prevShowParagraph => !prevShowParagraph);  // clean way to set oposit state
+        setShowParagraph(prevShowParagraph => !prevShowParagraph);  // clean way to set oposit state
     };
 
     return (
         <div className="app">
             <h1>Hi there!</h1>
-            <DemoOutput show={showParagraph} />
+            <DemoOutput show={false} />
             <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
         </div>
     );
--- src/components/Demo/DemoOutput.js
+++ src/components/Demo/DemoOutput.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const DemoOutput = props => {
-    console.log('DemoOutput running: every state change entire re-excuted, re-evaluated.')
-    // this message always appeared in cosole with App's message because props changed every click
+    console.log('DemoOutput running: every state change entire re-executed, re-evaluated.')
+    // this message always appeared in console with App's message because props changed every click
     // but, actually props values does NOT matter for re-eval, re-exc.
+    // just fact of parent changed.
 
     return <p>{props.show ? 'This is new' : ''}</p>
 };
 
-export default DemoOutput;
+// export default DemoOutput;
+export default React.memo(DemoOutput);
+// React.memo(component): 
+// if the parent component change, 
+// but, the props values for that this component did not change,
+// this component excution will be skipped.
